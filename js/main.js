ab00a2b131761713c0aa03a536b6b11182fe34e3
Rudy <38470001+rudypflores@users.noreply.github.com>
2018-11-21 23:55:26 -0600

    refactor rudy

--- js/main.js
+++ js/main.js
@@ -20,4 +20,4 @@ const openTab = (evt, tabName) => {
 }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpen").onclick();
